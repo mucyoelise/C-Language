6a156059e153fa13e0a32431be61e684f9c5cf85
mucyoelise <elisemcyo@gmail.com>
2025-01-28 18:36:27 +0200

    first-commit

---
+++ me/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void) {
+    char str[10];
+    printf("Enter a string: ");
+    scanf("%s", &str);  // Read a line of text
+    printf("You entered: %s\n", str);
+
+}
