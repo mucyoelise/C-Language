d4e988cc5a7ca454c7c3e2a3df8d1b8a5bbab0bf
mucyoelise <elisemcyo@gmail.com>
2025-05-11 14:10:57 +0200

    First-commit

--- probSet1/discrete/question1.c
+++ probSet1/discrete/question1.c
@@ -1,25 +1,32 @@
-#include<stdio.h>
-int fact(int n);
+#include <stdio.h>
 
-int main (){
-    int nbr, factorial;
-    next: ;
-    printf("Enter a number to find its factorial: ");
-    scanf("%d", &nbr);
-    if (nbr < 0){
-        printf("Can't find factorial of a negative number, please try again!\n");
-        goto next;
+// Recursive function to calculate factorial
+int fact(int n) {
+    // Base cases: 0! = 1, 1! = 1
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        // Recursive call is applied here
+        return n * fact(n - 1);
     }
-    factorial = fact(nbr);
-    printf("The factorial of the number(%d!): %d\n", nbr, factorial);
-    return 0;
 }
 
-int fact(int n){
-    if (n == 1 || n == 0){
-        return 1;
+int main() {
+    int nbr, factorial;
+
+    next: ; // Label for retrying input
+
+    printf("Enter a number to find its factorial: ");
+    scanf("%d", &nbr);
+
+    if (nbr < 0) {
+        // Factorial not defined for negative numbers
+        printf("Can't find factorial of a negative number, please try again!\n");
+        goto next; // Go back to ask again
     }
-    if (n>1){
-        return n * fact(n-1);
-    }
+
+    factorial = fact(nbr); // Function call
+    printf("The factorial of the number (%d!) is: %d\n", nbr, factorial);
+
+    return 0;
 }
